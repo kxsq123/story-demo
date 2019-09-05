d1fe1761c727f42b0a082e06d2d96f8515db3c4b
daizengyu <daizengyu@aiyunxiao.com>
2019-09-05 15:28:53 +0800

    preview head 集成 vue devtools (electron)

---
+++ .storybook/addons.js
@@ -0,0 +1,8 @@
+/**
+ * Created by daizengyu on 2019/3/26.
+ */
+
+import '@storybook/addon-storysource/register';
+import '@storybook/addon-actions/register';
+import '@storybook/addon-knobs/register';
+import '@storybook/addon-notes/register';
